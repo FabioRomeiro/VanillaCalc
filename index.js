d0ba623394701453f9641ec772f0798efc41c36a
FabioRomeiro <flromeiroc@gmail.com>
2019-01-07 04:57:25 -0200

    Adicioanndo process Env

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./config/custom-express');
 
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.listen(PORT, function() {
   console.log(`Server running at ${PORT}`)
